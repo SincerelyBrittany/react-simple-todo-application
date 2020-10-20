1f5fa88e180d5cbcc1a3f545608ca2db46c1de2a
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-20 10:06:02 -0400

    crates a TodoItem class

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 export default class TodoItem extends Component{
     render(){
         return(
-            <div>
-            <p> Placeholder text here </p>
-            <input type="checkbox"/>
+            <div className="todo-item">
+                <p>{this.props.todo}</p>
+            {/* <p> Placeholder text here </p>
+            <input type="checkbox"/> */}
             </div>
         )
     }
