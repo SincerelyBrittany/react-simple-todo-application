a2feaab016eafd0051738a4d65a1142dd24658a0
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-20 10:05:44 -0400

    adds an array of todo objects to display

---
+++ src/todoData.js
@@ -0,0 +1,28 @@
+const todoData = [
+    {
+        id: 1,
+        todo: "Go to the store"
+    },
+    {
+        id: 2,
+        todo: "Pick up the kids"
+    },
+    {
+        id: 3,
+        todo: "Code"
+    },
+    {
+        id: 4,
+        todo: "Go to work"
+    },
+    {
+        id: 5,
+        todo: "Drop off Laundry"
+    },
+    {
+        id: 6,
+        todo: "Cook"
+    }
+]
+
+export default todoData
