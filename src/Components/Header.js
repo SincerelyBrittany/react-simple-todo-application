aa56f7cff906db7921adc1a4ad5cef6597bc5477
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-20 10:04:34 -0400

    adds the header to display the time basd off if satment

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Header extends Component {
+    
     render(){
+        const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay
+
+        if (hours < 12){
+            timeOfDay = "morning"
+        } else if (hours >= 12 && hours < 17){
+            timeOfDay = "afternoon"
+        } else {
+            timeOfDay = "night"
+        }
+
         return(
-            <header className="navbar"> This is the header </header>
+            <div>
+            <header className="navbar"> Good {timeOfDay}, View Your List Below: </header>
+            {/* <h3 style={{color: "red", backgroundColor: "pink"}}> Good {timeOfDay} </h3> */}
+            </div>
         )
     }
 }
