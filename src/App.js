6bf45bc83cd619416079a31deb0a6f4d7f15fcc1
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-21 14:07:31 -0400

    adds compenent did mount to wait 3 seconds before loading the todos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import logo from './logo.svg';
 import Header from './Components/Header.js'
 import ListContainer from './Components/ListContainer.js'
 import Footer from './Components/Footer.js'
 import './styles/App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      })
+    }, 3000)
+  }
+
+  render(){
   return (
     <div className="App">
       {/* <h1>Your List</h1> */}
       < Header />
+      {this.state.isLoading ? 
+      <h1> Loading </h1> :
       < ListContainer />
+      }
       < Footer />
     </div>
   );
+  }
 }
 
 export default App;
