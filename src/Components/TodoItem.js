e1102f60c9e317af678ae5b5e9d2db1be8760fb3
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-21 14:53:50 -0400

    adds styles to todo items based off the checkmark

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react'
 
 export default class TodoItem extends Component{
     render(){
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "red",
+            textDecoration: "line-through"
+        }
         return(
             <div className="todo-item">
-            <p>{this.props.todo.id} </p>
+            <p style={this.props.todo.completed ? completedStyle: null}>{this.props.todo.todo} </p>
             <input type="checkbox" 
             checked={this.props.todo.completed} 
             onChange={() => this.props.handleChange(this.props.todo.id)}/> 
