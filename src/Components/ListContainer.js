81fee0e8f1f0e740a4adc3b65e2c9208646d92f8
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-20 20:21:49 -0400

    adds props o TodoItem

--- src/Components/ListContainer.js
+++ src/Components/ListContainer.js
@@ -6,7 +6,7 @@ export default class ListContainer extends Component {
     render(){
         const todoComponents = todoData.map(todo => {
             // console.log(todo.id)
-           return < TodoItem key={todo.id} todo={todo.todo} />
+           return < TodoItem key={todo.id} todo={todo}  />
         })
         return(
             <div className="todo-list">
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react'
 
 export default class TodoItem extends Component{
     render(){
+        console.log(this.props.todo.todo)
         return(
             <div className="todo-item">
-                <p>{this.props.todo}</p>
-            {/* <p> Placeholder text here </p>
-            <input type="checkbox"/> */}
+              
+                {/* <p style={{display: !this.props.todo && "none"}}>{this.props.todo}</p> */}
+                {/* <p>{this.props.todo}</p> */}
+                {this.props.todo.todo}
+            <input type="checkbox" checked={this.props.todo.completed}/> 
             </div>
         )
     }
 }
+
+// TodoItem.defaultProps = {
+//     todo: 'http://i.imgur.com/bJw8ndW.png'
+//   }
